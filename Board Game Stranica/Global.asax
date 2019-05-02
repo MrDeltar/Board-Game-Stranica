9f62ac08ae8c5b933207438773d9e4fef194b27d
Delta <jcilar6@gmail.com>
2019-05-02 19:14:58 +0200

    Add project files.

---
+++ Board Game Stranica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Board_Game_Stranica.MvcApplication" Language="C#" %>
---
+++ Board Game Stranica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Board_Game_Stranica.Startup))]
+namespace Board_Game_Stranica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
